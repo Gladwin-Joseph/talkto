ccf14aa42d45ff488cadbe63b46fbb3462b64604
Gladwin-Joseph <gladwinjoseph07@gmail.com>
2024-05-01 12:33:16 +0530

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -8,9 +8,9 @@ const Home = () => {
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
-        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11 mt-6 md:p-6">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11 mt-8 md:p-6">
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">{date}</h1>
+            <h1 className="text-4xl font-extrabold lg:text-6xl">{date}</h1>
           </div>
         </div>
       </div>
