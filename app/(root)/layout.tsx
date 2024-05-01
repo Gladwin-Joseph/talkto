720e2f93134caca71428d23b15098fb2c6e79666
Gladwin <gladwinjoseph07@gmail.com>
2024-05-01 11:39:18 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "TalkTo",
+  description: "Video calling web app",
+  icons: {
+    icon: "/icons/logo.svg"
+  }
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
